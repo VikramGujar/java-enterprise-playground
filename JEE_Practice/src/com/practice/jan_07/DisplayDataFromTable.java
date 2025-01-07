83e33c54e838c6291278d56b73318bd80af19c60
VikramGujar <vikramgujar300@gmail.com>
2025-01-07 23:37:43 +0530

    Employee databse management and Product management using jdbc with orcle databse

--- JEE_Practice/src/com/practice/jan_07/DisplayDataFromTable.java
+++ JEE_Practice/src/com/practice/jan_07/DisplayDataFromTable.java
@@ -15,19 +15,31 @@ public class DisplayDataFromTable
 
 		// Step-1 : Loading driver
 		// Path for Driver = "oracle.jdbc.driver.OracleDriver"
-		Class.forName("orcle.jdbc.driver.OracleDriver");
+		Class.forName("oracle.jdbc.driver.OracleDriver");
 		
 		//Step-2 : Creating Connection
-		//jdbc:oracle:thin:@localhost:1521:xe
-		//Username = system
-		//Password = tonystark
-		Connection con = DriverManager.getConnection(null)
+		//Database URL = jdbc:oracle:thin:@localhost:1521:xe
+		//Username     = system
+		//Password     = tonystark
+		//Connection con = DriverManager.getConnection(dburl,uname,pass);
+		Connection con = DriverManager.getConnection
+				("jdbc:oracle:thin:@localhost:1521:xe","system","tonystark");
 		
 		// Step-3 : Creating JDBC-statement
-		
+		Statement st = con.createStatement();
 		
 		//Step-4 : Executing query
+		//Execute query for select operations in SQL
+		ResultSet rs = st.executeQuery("select * from employees");
 		
+		while(rs.next())
+		{
+			int id = rs.getInt(1);
+			String name = rs.getString(2);
+			double salary = rs.getDouble(3);
+			
+			System.out.println("Employee : [Id :"+id+"\t Name :"+name+"\t Salary :"+salary+"]");
+		}
 		
 		// Step-5 : Closing the Connection
 		}catch (Exception e) 
